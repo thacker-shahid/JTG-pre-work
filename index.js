a736bde7b0efb175726300e22d53739bc5241a96
Teekam Singh <teekams10999@gmail.com>
2021-07-16 18:20:01 +0530

    final zip

---
+++ index.js
@@ -0,0 +1,13 @@
+// ========== Working slider ================
+
+$('.icons-slider').slick({
+    slidesToShow:6,
+    slidesToScroll:1,
+    dots:false,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='images/Vector2.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='images/Vector3.png'>",
+    arrows:true,
+    autoplay:true,
+    autoplaySpeed:1500,
+    infinite:true
+  });
